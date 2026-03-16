314f3b9caecbbd10952099194459b19677f3f68f
Thibaut-Renaut <renautthibaut4@gmail.com>
2026-03-16 10:51:21 +0100

    Initial commit : ajout du frontend, backend et docker-compose

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = 3000;
+
+const app = express();
+
+// Autoriser le frontend à appeler cette API
+app.use(cors());
+
+app.get("/api/status", function (req, res) {
+  res.json({
+    status: "success",
+    message: "Le backend est connecté !",
+  });
+});
+
+app.listen(PORT, function () {
+  console.log("Backend Server started on port", PORT);
+});
